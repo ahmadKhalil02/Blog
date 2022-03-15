8270ed83ad1665e7ab7af9be5479b64782962884
Ahmad Khalil <khalilahmad0205@gmail.com>
2022-03-15 16:41:19 +0100

    Påbörjad CSS

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -6,26 +6,21 @@ async function fetchallBlogs(){
     try{
         const response = await fetch ('http://localhost:5000/posts')
         const blogs = await response.json();
-        console.log(blogs)
+        // console.log(blogs)
     
         let html = ''
         for(let blog of blogs){
+        var temp = "";
         html += `
-        <table>
-        <tr>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Date</th>
-          <th>Action</th>
-        </tr>
-        <tr>
+
+      
           <td>${blog.title}</td>
           <td>${blog.author}</td>
           <td>${blog.date}</td>
           <td><a href="update-post.html?id=${blog.id}">Update</a> + <a href="#">Delete</a></td> 
-        </tr>
-      </table>
+ 
         `
+      
 
         console.log(blog.date)
     }
