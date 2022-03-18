349abf53df57f77c69f086af08fea864d8a0f6b9
Miggorie <info@elinselin.com>
2022-03-18 15:05:09 +0100

    update css for post and index

--- js/post.js
+++ js/post.js
@@ -1,47 +1,36 @@
 window.onload = function () {
-  getSpecificPost();
-};
+  console.log(window.location.search);
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  console.log(urlParams.get("id"));
 
-type blogPost = {
-  id: String,
-  name: String,
-};
+  fetchPost(urlParams);
+  /** updatePostEvent(urlParams); */
 
-let url = "http://localhost:5000/posts";
+  async function fetchPost(urlParams) {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/posts/${urlParams.get("id")}`
+      );
+      const post = await response.json();
+      console.log(post);
 
-async function getAllPosts() {
-  try {
-    const response = await fetch("http://localhost:5000/posts${post._id}");
-    const posts = await response.json();
-    console.log(posts);
+      let html = "";
+      html += ` 
+                <div class="list-group-item">
+                    <h3>${post.title}</h3><br>
+                    <span class="date">Author: ${post.author} <br> Date: ${post.date} </span><br>
+                    
+                    <div class="flexbox"><p><br>${post.content}<br>
 
-    let html = "";
-    for (let post of posts) {
-      let tags = post.tags;
-
-      if (!Array.isArray(tags) || !tags.length) {
-        tags = "";
-      }
-
-      html += `
-                <li class="list-item">
-                <h3>${post.title}</h3><br><span class="date">Author: ${
-        post.author
-      } <br> Date: ${
-        post.date
-      } </span><br> <div class="flexbox"><p><br>${post.content
-        .split(" ", 20)
-        .join(" ")} <br><a href ="http://localhost:5000/posts/${
-        post._id
-      }">Läs mer...</a>
-      <p class ="taggar">Tags: ${tags}</p>
-      </div></li>
-                
+                    <p class="post-taggar">Tags: ${post.tags}</p> 
+                    
+                </div>
             `;
-    }
 
-    document.getElementById("post-list").innerHTML = html;
-  } catch (error) {
-    console.log(error);
+      document.getElementById("post-list").innerHTML = html;
+    } catch (error) {
+      console.log(error);
+    }
   }
-}
+};
